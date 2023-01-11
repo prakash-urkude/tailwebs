78f069e85d89963787ede83c3c6c1ca80b9369c8
prakash-urkude <prakashurkude42416@gmail.com>
2023-01-12 01:24:01 +0530

    add

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -7,12 +7,17 @@ const isValid = mongoose.Type.ObjectId.isValid
 const createStudent = async function(req,res){
     try{
         const data =req.body
-        const { name, rollno, subject } = data
+        let userId = req.params.userId
+        const { name, rollno, subject, marks } = data
         if(Object.keys(data).length == 0) return res.status(400).send("please provide data in body")
+
+        let User=await teacherModel.findById({_id:userId})
+        if(!User) return res.status(404).send({status:false,message:"user not found"})
 
         if(!name) return res.status(400).send({ status: false, msg: "Please provide the title"})
         if(!rollno) return res.status(400).send({ status: false, msg: "Please provide the rollno"})
-        if(!subject) return res.status(400).send({ status: false, msg: "Please provide the subject"})    
+        if(!subject) return res.status(400).send({ status: false, msg: "Please provide the subject"})
+        if(!marks) return res.status(400).send({ status: false, msg: "Please provide the marks"})    
 
         const user = userModel.create(data)
         return res.status(201).send({status:true,data:user})
@@ -40,6 +45,27 @@ catch(err){
     return res.status(500).send({ status: false, message: err.message })
 }}
 
+const updateStudent = async function(req,res){
+    try{
+        let studentId = req.params.studentId
+        let data = req.body
+        let {name , subject, rollno ,marks} =data
+
+        if(!isValid(studentId)) return res.status(400).send({status:false,message:"ID is invalid"})
+        let student= await studentModel.findById({studentId})
+        if(!student) return res.status(400).send("student not present with this id")
+        
+        if(Object.keys(data).length==0) return res.status(400).send("please provide data in body")
+        else{if(name && subject){
+            let updateStudent= await studentModel.findOne({_id:studentId,name:name,subject:subject,isDeleted:false},{marks:(student.marks+marks)},{new:true})
+            return res.status(200).send({status:true,message:"Marks added",data:updateStudent})
+        }else{let createStud=await studentModel.create(data)
+            return res.status(201).send({status:true,message:"Success",data:createStud})}}}
+catch(err){
+        return res.status(500).send({status:false,message:error.messgae})
+    }
+}
+
 const deleteStudent=async function(req,res){
     try{
         let studentId=req.params.studentId
@@ -55,4 +81,4 @@ const deleteStudent=async function(req,res){
     }
     }
 
-module.exports ={createStudent,getStudents,deleteStudent}
+module.exports ={createStudent,getStudents,updateStudent,deleteStudent}
--- src/route/route.js
+++ src/route/route.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const userController=require('../Controller/teacherController')
+const studentController=require('../Controller/studentController')
+const auth=require('../middleware/auth')
 
+router.post('/createUser',createTeacher)
+router.post('/login',login)
+
+router.post('/createstudent/:userId',auth.authentication,studentController.createStudent)
+router.get('/getstudent',auth.authentication,studentController.getStudents)
+router.put('/updatemarks/:studentId',auth.authentication,studentController.updateStudent)
+router.delete('/deletestudent/:studentId',auth.authentication,studentController.deleteStudent)
+
+router.all('/*',function(req,res){
+    return res.status(404).send({messgae:"Invalid http request"})
+})
 
 
 module.exports = router
